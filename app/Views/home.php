ed43c872547ccc403a81445c5d6b2267c906c12d
KaibaBenja <sanchezmoralesbenjamin10@gmail.com>
2025-04-16 19:27:43 -0300

    Add: Config proyect

---
+++ app/Views/home.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layout/layout') ?>
+
+<?= $this->section('title') ?>
+Home
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, impedit! Sint, dicta! Nobis quam doloribus delectus nemo possimus in, nostrum cupiditate, officia quisquam tempore vitae inventore quia aut autem nesciunt.</p>
+<?= $this->endSection() ?>
